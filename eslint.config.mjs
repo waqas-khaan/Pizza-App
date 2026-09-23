b3af15519ed6eff49d8fbf1c968995ddea0a1525
Muhammad Waqas <waqas.erio@gmail.com>
2026-09-23 19:17:19 +0500

    setting up eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig({
+    files: ["**/*.{js,ts}"],
+    extends: [js.configs.recommended, tseslint.configs.recommendedTypeChecked],
+    ignores: ["dist", "node_modules"],
+    languageOptions: {
+        parserOptions: {
+            projectService: true,
+        },
+    },
+    rules: {
+        // "no-console": "error",
+        "dot-notation": "error",
+    },
+});
