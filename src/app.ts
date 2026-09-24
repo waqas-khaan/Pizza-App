28bca3500bea743a02c88efcea53d7ba185b28a3
Muhammad Waqas <waqas.erio@gmail.com>
2026-09-25 03:05:23 +0500

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,31 @@
-import express from "express";
+import express, {
+    type NextFunction,
+    type Request,
+    type Response,
+} from "express";
+import type { HttpError } from "http-errors";
+import logger from "./config/logger.js";
+import createHttpError from "http-errors";
+
 const app = express();
-app.get("/", (req, res) => {
-    res.send(`Welcome to Auth Service`);
+app.get("/", (req, res, next) => {
+    const err = createHttpError(401, "You do not have access");
+    next(err);
+    // res.send(`Welcome to Auth Service`);
+});
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 export default app;
